872d3a079b92ea77ccce59fc79cf52883e4a3b7b
Scottsdaaale <scottsdaaale@gmail.com>
2023-10-30 22:23:02 -0400

    created custom accordion

--- src/components/AccordionMenu.jsx
+++ src/components/AccordionMenu.jsx
@@ -1,134 +1,68 @@
+// import Accordion from "react-bootstrap/Accordion";
 
-import Accordion from 'react-bootstrap/Accordion';
-import Card from 'react-bootstrap/Card';
+// function AccordionMenu({ items }) {
+//   return (
+//     <Accordion defaultActiveKey="0">
+//       {items.map((item, index) => (
+//         <Accordion.Item
+//           key={index}
+//           eventKey={index.toString()}
+//           className="mb-3 rounded-pill"
+//         >
+//           <Accordion.Header>{item.title}</Accordion.Header>
+//           <Accordion.Body>{item.content}</Accordion.Body>
+//         </Accordion.Item>
+//       ))}
+//     </Accordion>
+//   );
+// }
+
+// export default AccordionMenu;
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCaretDown } from '@fortawesome/free-solid-svg-icons';
 
 function AccordionMenu({ items }) {
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  const isItemActive = (index) => index === activeIndex;
+
   return (
-    <Accordion defaultActiveKey="0" >
+    <div className="custom-accordion">
       {items.map((item, index) => (
-        <Card key={index} >
-          <Accordion.Item eventKey={index.toString()} >
-            <Accordion.Header >{item.title}</Accordion.Header>
-            <Accordion.Body>{item.content}</Accordion.Body>
-          </Accordion.Item>
-        </Card>
+        <div
+          key={index}
+          className={`accordion-item ${isItemActive(index) ? 'active' : ''}`}
+          style={{marginBottom: "20px", borderColor: "white"}}
+        >
+          <div
+            className="accordion-header"
+            onClick={() => {
+              setActiveIndex(isItemActive(index) ? null : index);
+            }}
+            style={{ color: 'white' }}
+          >
+            <span>{item.title}</span>
+            <span
+              className={`accordion-icon ${isItemActive(index) ? 'rotate' : ''}`}
+            >
+              <FontAwesomeIcon icon={faCaretDown} />
+            </span>
+          </div>
+          <div
+            className="accordion-content"
+            style={{
+              maxHeight: isItemActive(index) ? '1000px' : '0',
+              transition: `max-height ${isItemActive(index) ? '0.3s' : '0.1s'} ease-in-out`,
+            }}
+          >
+            {item.content}
+          </div>
+        </div>
       ))}
-    </Accordion>
+    </div>
   );
 }
 
 export default AccordionMenu;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React, { useState } from 'react';
-
-// const Accordion = () => {
-//   const [activeSection, setActiveSection] = useState(null);
-
-//   const handleSectionClick = (sectionName) => {
-//     if (activeSection === sectionName) {
-//       // Clicking the active section again will close it.
-//       setActiveSection(null);
-//     } else {
-//       setActiveSection(sectionName);
-//     }
-//   };
-
-//   return (
-//     <div>
-//       <div className="accordion-section">
-//         <div
-//           className={`accordion-title ${activeSection === 'videos' ? 'active' : ''}`}
-//           onClick={() => handleSectionClick('videos')}
-//         >
-//           Videos
-//         </div>
-//         {activeSection === 'videos' && (
-//           <div className="accordion-content">
-//             hello
-//           </div>
-//         )}
-//       </div>
-
-//       <div className="accordion-section">
-//         <div
-//           className={`accordion-title ${activeSection === 'background' ? 'active' : ''}`}
-//           onClick={() => handleSectionClick('background')}
-//         >
-//           Background
-//         </div>
-//         {activeSection === 'background' && (
-//           <div className="accordion-content">
-//             bass
-//           </div>
-//         )}
-//       </div>
-
-//       <div className="accordion-section">
-//         <div
-//           className={`accordion-title ${activeSection === 'playlists' ? 'active' : ''}`}
-//           onClick={() => handleSectionClick('playlists')}
-//         >
-//           Playlists
-//         </div>
-//         {activeSection === 'playlists' && (
-//           <div className="accordion-content">
-//             {/* Add content for the Playlists section here */}
-//           </div>
-//         )}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Accordion;
