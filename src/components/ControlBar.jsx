77f4ad88b95ae0d1064f6d7f172a2f306c36b2e8
Scottsdaaale <scottsdaaale@gmail.com>
2023-11-02 14:49:36 -0400

    add controlbar

---
+++ src/components/ControlBar.jsx
@@ -0,0 +1,15 @@
+function ControlBar() {
+  return (
+    <div
+      style={{
+        border: "1px solid white",
+        color: "white",
+        padding: "10px"
+      }}
+    >
+      ControlBar
+    </div>
+  );
+}
+
+export default ControlBar;
